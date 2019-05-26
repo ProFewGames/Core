3d548599e4092582fb006d3e8d6038cc1055a8d5
ProFewGames <rickbrook247@gmail.com>
2019-05-26 17:41:40 -0400

    Update ColorModule.java

--- src/main/java/xyz/ufactions/chatcolor/ColorModule.java
+++ src/main/java/xyz/ufactions/chatcolor/ColorModule.java
@@ -18,6 +18,8 @@ import java.util.List;
 
 public class ColorModule extends Module {
 
+    private String testing = "testing";
+    
     private HashMap<Player, ChatColor> colors = new HashMap<>();
 
     public ColorModule(JavaPlugin plugin) {
